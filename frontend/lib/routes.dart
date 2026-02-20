6587d47e6f5e0d94c579b9e1175a747973df35cb
Paulo Oliveira <pv.oliveira@live.com>
2026-02-20 17:25:54 -0300

    feat: Initialize Flutter project with multi-platform setup

---
+++ frontend/lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:go_router/go_router.dart';
+import 'package:frontend/view/users/users_view.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [GoRoute(path: '/', builder: (context, state) => const UsersView())],
+);
